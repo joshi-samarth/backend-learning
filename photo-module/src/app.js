d1c4a3a1ccf034ca34e9ab4cfb8373ca4367d052
joshi samarth <samarth31j@gmail.com>
2026-05-20 01:02:44 +0530

    donw with imagekit with backend

--- photo-module/src/app.js
+++ photo-module/src/app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const multer = require('multer')
 const uploadFile = require('./services/storage.service')
+const postModel=require('./model/post.model')
 
 const app = express();
 
@@ -10,8 +11,25 @@ const upload = multer({ storage: multer.memoryStorage() })
 
 app.post('/upload/files', upload.single('image'), async (req, res) => {
     const result = await uploadFile(req.file.buffer);
-    console.log(result);
-    res.json(result);
+    
+    const post= await postModel.create({
+        image:result.url,
+        caption: req.body.caption
+    })
+
+    return res.status(201).json({
+        message:"post created"
+    })
+})
+
+app.get('/upload/get',async (req,res)=>{
+    const show= await postModel.find();
+
+    return res.status(200).json({
+        message:"data send successfully",
+        show
+
+    })
 })
 
 
