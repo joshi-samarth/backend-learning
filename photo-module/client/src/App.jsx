8e9d7f163718325e86e23d9ee4f2ddb904027cc0
joshi samarth <samarth31j@gmail.com>
2026-05-21 17:16:58 +0530

    added

---
+++ photo-module/client/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import {BrowserRouter as router ,Route , Router, Routes} from 'react-router-dom'
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path='' element={}></Route>
+      </Routes>
+    </Router>
+  )
+}
+
+export default App
--- photo-module/server/server.js
+++ photo-module/server/server.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
-const app = require('./src/app');
-const connect = require('./src/db/db')
+const app = require('../src/app');
+const connect = require('../src/db/db')
 
 connect();
 
